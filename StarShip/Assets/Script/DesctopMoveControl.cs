be39c95cf8b91451fa6511c74b54317c525d46a5
Eugenie Chupris <zhenuh15@gmail.com>
2020-01-29 11:56:40 +0300

    Add Shot in PlayerShip

--- StarShip/Assets/Script/DesctopMoveControl.cs
+++ StarShip/Assets/Script/DesctopMoveControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DesctopMoveControl : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 8f;
     // Start is called before the first frame update
     void Start()
     {
---
+++ StarShip/Assets/Script/ShotScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public GameObject ammo;
+    public float reloadSpeed = 0.5f;
+    bool CanShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanShot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanShot)
+            StartCoroutine(Shot());
+    }
+
+    IEnumerator Shot()
+    {
+        Instantiate(ammo,gameObject.transform);
+        CanShot = false;
+        yield return new WaitForSeconds(reloadSpeed);
+        CanShot = true;
+    }
+}
